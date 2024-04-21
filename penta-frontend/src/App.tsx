12316e11beecb86f8082f2e5d4d21bfdb4ac87f7
Nikita <manunknown964@gmail.com>
2024-04-21 12:04:30 +0500

    Added some cards, created footer component

--- penta-frontend/src/App.tsx
+++ penta-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import About from "./components/about/About";
 import Contacts from "./components/contacts/Contacts";
 import Header from "./components/header/Header";
 import Main from "./components/Main";
+import Footer from "./components/footer/Footer";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contacts" element={<Contacts />} />
         </Routes>
+        <Footer />
       </div>
     </>
   )
@@ -24,6 +26,7 @@ const centerContainer = {
   width: "70%",
   margin: "0 auto",
   maxWidth: "1920px",
+  height: "100%"
 }
 
 export default App
--- penta-frontend/src/components/Main.tsx
+++ penta-frontend/src/components/Main.tsx
@@ -1,11 +1,36 @@
 import './Components.scss'
-import logo from "../../assets/img/Logo.png"
+import logo from "../assets/img/Logo.png"
 
 export default function Main(){
 
+    let countProjects = "50+";
+
+    const whatYouGet = [
+        `Проекты, которыми пользуются: ${countProjects}`,
+        "Сложные и интересные задачи",
+        "Бесплатная практика на реальных проектах"
+    ]
+
     return(
         <>
-            <p>Main</p>
+            <div className="main-page">
+                <div className="big-card">
+                    <div className="blur-circle"></div>
+                    <img src={logo} alt="PentaPulse Logo" />
+                    <div className="text-box">
+                        <h2>PentaPulse</h2>
+                        <p>IT-команда</p>
+                    </div>
+                </div>
+                <h3 style={{marginLeft: "2rem"}}>От нас:</h3>
+                <div className="cardholder">
+                    {whatYouGet.map((e, i) => 
+                        <div key={i} className="card">
+                            <h4>{e}</h4>
+                        </div>
+                    )}
+                </div>
+            </div>
         </>
     );
 }
